110deef77f1b9f4379bcdd6b4ae1768d45b095f4
db <db@qq.com>
2023-01-14 23:12:59 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;/**
+ * @author DB1412
+ * @create 2023-01-14 23:09
+ */
+
+/**
+ *@ClassName GitTest
+ *@Description TODO
+ *@Author DB1412
+ *@Date 2023-01-14 23:09
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
